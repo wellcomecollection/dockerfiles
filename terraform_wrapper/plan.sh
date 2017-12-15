5a13d7725d38c73217befc28714c4037576aad4b
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-15 15:05:31 +0000

    Width to match terraform output

--- terraform_wrapper/plan.sh
+++ terraform_wrapper/plan.sh
@@ -20,13 +20,13 @@ terraform plan -out terraform.plan
 
 if [[ "${IS_PUBLIC_FACING:-true}" == "true" ]]
 then
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  echo "@                                                         @"
-  echo "@ Please review this plan.                                @"
-  echo "@                                                         @"
-  echo "@ Because changes in this stack may cause public-facing   @"
-  echo "@ changes, ask somebody else to double-check the plan     @"
-  echo "@ before you run the apply step.                          @"
-  echo "@                                                         @"
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@                                                                      @"
+  echo "@ Please review this plan.                                             @"
+  echo "@                                                                      @"
+  echo "@ Because changes in this stack may cause public-facing                @"
+  echo "@ changes, ask somebody else to double-check the plan                  @"
+  echo "@ before you run the apply step.                                       @"
+  echo "@                                                                      @"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 fi
