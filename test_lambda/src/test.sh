e1ae6ac5260501436b13c973a590eb95f49b2cd6
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-08 15:41:36 +0000

    Run verbose tests for easier debugging

--- test_lambda/src/test.sh
+++ test_lambda/src/test.sh
@@ -9,4 +9,4 @@ FIND_MATCH_PATHS=$1
 echo "FIND_MATCH_PATHS=$FIND_MATCH_PATHS"
 
 # Run tests
-find $1 -maxdepth 1 -name "test_*.py" | xargs py.test
+find $1 -maxdepth 1 -name "test_*.py" | xargs py.test --verbose
