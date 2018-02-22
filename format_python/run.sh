07ac7fce02b80fd72844eca50b02b325859e471c
Alex Chan <a.chan@wellcome.ac.uk>
2018-02-22 15:37:02 +0000

    format_python: Scale back the scope of formatting

--- format_python/run.sh
+++ format_python/run.sh
@@ -8,5 +8,4 @@ PATHS_TO_FORMAT=$(find . -name '*.py' \
   -not -path '*/.terraform/*' \
   -not -path '*/target/*')
 
-echo $PATHS_TO_FORMAT | xargs isort --line-width 79 --multi-line 2
-echo $PATHS_TO_FORMAT | xargs pyformat --aggressive --recursive --in-place
+echo $PATHS_TO_FORMAT | xargs autoflake --remove-all-unused-imports
