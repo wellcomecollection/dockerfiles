48f2bd9ede4c4316175c73cb44626008e78ad794
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-11 16:11:17 +0100

    Actually, skip short circuiting and the dependency tree

--- run_travis.py
+++ run_travis.py
@@ -24,40 +24,11 @@ def get_docker_dirs():
                 yield root
 
 
-def git(*args):
-    """Run a Git command and return its output."""
-    cmd = ["git"] + list(args)
-    try:
-        return subprocess.check_output(cmd).decode("utf8").strip()
-    except subprocess.CalledProcessError as err:
-        print(err)
-        sys.exit(err.returncode)
-
-
-def get_changed_paths(*args):
-    """
-    Returns a set of changed paths in a given commit range.
-
-    :param commit_range: Arguments to pass to ``git diff``.
-    """
-    diff_output = git("diff", "--name-only", *args)
-
-    return set([line.strip() for line in diff_output.splitlines()])
-
-
 if __name__ == '__main__':
     try:
         task = sys.argv[1]
     except IndexError:
         sys.exit("Usage: %s (build | publish)" % __file__)
-
-    travis_event_type = os.environ["TRAVIS_EVENT_TYPE"]
-
-    if travis_event_type == "pull_request":
-        changed_paths = get_changed_paths("HEAD", "master")
-    else:
-        git("fetch", "origin")
-        changed_paths = get_changed_paths(os.environ["TRAVIS_COMMIT_RANGE"])
 
     for dirname in get_docker_dirs():
         print(dirname)
