29ada5404fe68c849ed3d5fd37fdd09de880058f
Alex Chan <alex@alexwlchan.net>
2019-01-28 11:51:16 +0000

    Don't fetch terraform vars from S3 any more

--- terraform_wrapper/app/plan.sh
+++ terraform_wrapper/app/plan.sh
@@ -3,24 +3,6 @@
 set -o errexit
 set -o nounset
 
-if [[ "${GET_TFVARS:-false}" == "true" ]]
-then
-  /app/get_tfvars.py
-fi
-
 terraform init
 terraform get
 terraform plan -out terraform.plan
-
-if [[ "${IS_PUBLIC_FACING:-true}" == "true" ]]
-then
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  echo "@                                                                      @"
-  echo "@ Please review this plan.                                             @"
-  echo "@                                                                      @"
-  echo "@ Because changes in this stack may cause public-facing                @"
-  echo "@ changes, ask somebody else to double-check the plan                  @"
-  echo "@ before you run the apply step.                                       @"
-  echo "@                                                                      @"
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-fi
