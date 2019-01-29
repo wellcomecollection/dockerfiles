4db2c446b9208e9922fb4a4dae1be3fd4ddef33c
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-29 12:34:26 +0000

    Don't log the rendered config (and secrets!) in our Finatra apps

--- finatra_service_base/run.sh
+++ finatra_service_base/run.sh
@@ -4,10 +4,5 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-echo "Substitute environment variables into /opt/docker/conf/application.ini.template"
 envsubst < /opt/docker/conf/application.ini.template > /opt/docker/conf/application.ini
-
-echo "Config: /opt/docker/conf/application.ini ==="
-cat /opt/docker/conf/application.ini
-
 /opt/docker/bin/${project}
